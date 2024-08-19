6b6c1566f89904ab5a4a8a0b484b93b8b7d1913d
ahmedjoubest <ahmedjoubest@gmail.com>
2024-08-19 16:41:39 +0200

    use |>

--- server.R
+++ server.R
@@ -56,17 +56,17 @@ server <- function(input, output, session) {
   
   # ------ * Initialize the line chart -----------------------------------------
   output$hc_addpoint <- renderHighchart({
-    hc <- highchart() %>%
-      hc_chart(type = "column") %>%
-      hc_xAxis(title = list(), labels = list(enabled = FALSE), tickLength = 0) %>%
+    hc <- highchart() |>
+      hc_chart(type = "column") |>
+      hc_xAxis(title = list(), labels = list(enabled = FALSE), tickLength = 0) |>
       hc_tooltip(
         headerFormat = "",  # Hides the tooltip title
         shared = TRUE
-      ) %>%
-      hc_title(text = NULL) %>% 
-      hc_colors(colors) %>%
-      hc_legend(enabled = FALSE) %>% 
-      hc_navigator(enabled = TRUE, adaptToUpdatedData = TRUE, height = 0) %>%
+      ) |>
+      hc_title(text = NULL) |> 
+      hc_colors(colors) |>
+      hc_legend(enabled = FALSE) |> 
+      hc_navigator(enabled = TRUE, adaptToUpdatedData = TRUE, height = 0) |>
       hc_plotOptions(
         column = list(
           states = list(
@@ -76,7 +76,7 @@ server <- function(input, output, session) {
       )
     # Add series for each visitor type
     for (type in c("New Visitor", "Returning Visitor")) {
-      hc <- hc %>%
+      hc <- hc |>
         hc_add_series(
           name = type,
           data = list_parse2(
@@ -97,14 +97,14 @@ server <- function(input, output, session) {
       isolate(tables$visits_dist), # isolate reactive data: only update through proxy
       "column",
       hcaes(x = pages, y = n_active_visits, group = visit_type)
-    ) %>%
-      hc_xAxis(title = list(text = NULL)) %>%
-      hc_colors(colors) %>%
+    ) |>
+      hc_xAxis(title = list(text = NULL)) |>
+      hc_colors(colors) |>
       hc_yAxis_multiples(
         list(title = list()),
         list(title = list(), opposite = TRUE)
-      ) %>%
-      hc_tooltip(shared = TRUE) %>%
+      ) |>
+      hc_tooltip(shared = TRUE) |>
       hc_plotOptions(
         column = list(
           states = list(
@@ -117,7 +117,7 @@ server <- function(input, output, session) {
   # ------ * Update the charts -------------------------------------------------
   observeEvent(tables$active_visits, {
     # update the line chart
-    highchartProxy("hc_addpoint") %>%
+    highchartProxy("hc_addpoint") |>
       hcpxy_add_point(
         id = "New Visitor",
         point = list(
@@ -125,7 +125,7 @@ server <- function(input, output, session) {
           y = tables$active_visits$n_active_visits[nrow(tables$active_visits) - 1]
         ),
         shift = TRUE
-      ) %>%
+      ) |>
       hcpxy_add_point(
         id = "Returning Visitor",
         point = list(
@@ -136,7 +136,7 @@ server <- function(input, output, session) {
       )
     
     # Update the bar chart
-    highchartProxy("hc_set_data") %>%
+    highchartProxy("hc_set_data") |>
       hcpxy_set_data(
         type = "column",
         data = tables$visits_dist,
